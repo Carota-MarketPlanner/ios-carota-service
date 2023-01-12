a9c589320e2876d6ae9ef89ff98278edd326889b
Elias Ferreira <eliasferreira.pro@gmail.com>
2023-01-11 22:54:51 -0300

    reset project example

---
+++ Example/MPServiceExample/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MPService
+//
+//  Created by Elias Ferreira on 11/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
